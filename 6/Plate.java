d41786f61908fdf24832c41cbf353d2c8f45d565
Albert Vagapov <va3@list.ru>
2023-03-08 14:07:08 +0500

    HW6 done

---
+++ 6/Plate.java
@@ -0,0 +1,29 @@
+
+public class Plate {
+
+    public int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public boolean increaseFood(int x) {
+        food = food + x;
+        return true;
+    }
+
+    public boolean decreaseFood(int foodToDecrease) {
+        if (food >= foodToDecrease) {
+            food = food - (foodToDecrease);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Plate[" + food + "]";
+    }
+
+}
